aca530afbe1db23a47e5e0b76b4737dfb141f94a
Daniel Rohr <daniel.rohr@w.roteskreuz.at>
2021-10-27 00:48:22 +0200

    Use environment variable for m2 settings file in Docker workflow

--- docker-build.sh
+++ docker-build.sh
@@ -7,6 +7,6 @@ app=auth-service
 echo "Running Docker build for ${app} with secrets from '${settings}'..."
 
 DOCKER_BUILDKIT=1 docker build \
-    --secret id=m2-settings,src=m2-settings.xml \
+    --secret id=m2-settings,src="${settings}" \
     --build-arg MODULE=$module --build-arg APP=$app \
     .
