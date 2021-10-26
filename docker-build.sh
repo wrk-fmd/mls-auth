a0e460e22346e0ac5a497100406c7d019759e70c
Daniel Rohr <daniel.rohr@w.roteskreuz.at>
2021-10-27 00:44:49 +0200

    Add Docker build workflow

---
+++ docker-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+settings=${MLS_M2_SETTINGS:-~/.m2/settings.xml}
+module=auth-service
+app=auth-service
+
+echo "Running Docker build for ${app} with secrets from '${settings}'..."
+
+DOCKER_BUILDKIT=1 docker build \
+    --secret id=m2-settings,src=m2-settings.xml \
+    --build-arg MODULE=$module --build-arg APP=$app \
+    .
