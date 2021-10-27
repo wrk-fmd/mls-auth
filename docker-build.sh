2a1677a03d269d65252a5236d3c660cc9125ea22
Daniel Rohr <daniel.rohr@w.roteskreuz.at>
2021-10-27 02:12:11 +0200

    Tag docker image when using build script

--- docker-build.sh
+++ docker-build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# This script can be used to build the Docker image locally, since `docker-compose build` does not support Buildkit secrets
+
 settings=${MLS_M2_SETTINGS:-~/.m2/settings.xml}
 module=auth-service
 app=auth-service
+tag=wrkfmdit/mls-auth:${MLS_TAG:-latest}
 
 echo "Running Docker build for ${app} with secrets from '${settings}'..."
 
 DOCKER_BUILDKIT=1 docker build \
     --secret id=m2-settings,src="${settings}" \
     --build-arg MODULE=$module --build-arg APP=$app \
-    .
+    -t "$tag" .
